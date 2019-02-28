72157c1b53382c7b7f9b568578bda61fe7eb7e8c
timrooke1991 <timrooke1991@gmail.com>
2019-02-28 22:09:07 +0000

    Changed to smoother animation using position absolute

--- src/components/description.js
+++ src/components/description.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Description = ({ heading, subHeading, paragraph }) => (
-  <React.Fragment>
+  <div className="description-block">
     <h3 className="darkText">{heading}</h3>
     {subHeading ? <h4>{subHeading}</h4> : null}
     <p>{paragraph}</p>
-  </React.Fragment>
+  </div>
 );
 
 Description.propTypes = {
